bc1bcf661c70649d530cfe2ae1a8f78ec8ec0946
Vishal Jaiswal <vishal@invisibl.io>
2023-05-24 17:35:16 +0530

    ,

--- script/compute-post-install.sh
+++ script/compute-post-install.sh
@@ -8,8 +8,13 @@
 
 #Load AWS Parallelcluster environment variables
 . /etc/parallelcluster/cfnconfig
-monitoring_dir_name=aws-parallelcluster-monitoring
+# monitoring_dir_name=aws-parallelcluster-monitoring
+
 install_monitoring_script=https://raw.githubusercontent.com/vishJ26/slurm-monitoring/master/script/compute-node.sh
+
+mkdir compute-node-scripts
+cd compute-node-scripts
+curl -o compute-node.sh $install_monitoring_script
 
 # #get GitHub repo to clone and the installation script
 # setup_command=install-monitoring.sh
@@ -17,5 +22,5 @@ install_monitoring_script=https://raw.githubusercontent.com/vishJ26/slurm-monito
 
 #Execute the monitoring installation script
 # bash -x "${monitoring_home}/parallelcluster-setup/${setup_command}" >/tmp/monitoring-setup.log 2>&1
-bash -x "${install_monitoring_script}" >/tmp/monitoring-setup.log 2>&1
+bash -x compute-node.sh >/tmp/monitoring-setup.log 2>&1
 exit $?
