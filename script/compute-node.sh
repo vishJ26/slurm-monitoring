27a8fa8623f08306e89ab9ab0f143e5da202b845
vishJ26 <99247415+vishJ26@users.noreply.github.com>
2023-07-01 18:26:57 +0530

    Update compute-node.sh

--- script/compute-node.sh
+++ script/compute-node.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-# # #source the AWS ParallelCluster profile
-. /etc/parallelcluster/cfnconfig
+./etc/parallelcluster/cfnconfig
 
-# mkdir /shared/vishal/script-test-folder
-cd /shared/vishal/
+s3_bucket_name=vishal-application
+region=us-east-1
+S3FileName=pcluster/cw-computenode.json
+# dir=cw-configs
 
-cat >> strings.text << EOF
-text
-more text
-and another line
-EOF
+cd /shared/cw-configs
+
+aws s3api get-object --bucket $s3_bucket_name --region $region --key $S3FileName  cw-computenode.json
+
+# replace queue name in file
+sed -i "s/__QUEUE_NAME__/${cfn_scheduler_queue_name}/g"  	cw-computenode.json
+
+# add config to cloudwatch
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a append-config -m ec2 -s -c file:cw-computenode.json
+
