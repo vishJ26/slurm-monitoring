6d9c51192490b956e40828695cba0a0a5f675d45
vishJ26 <99247415+vishJ26@users.noreply.github.com>
2023-07-01 19:30:10 +0530

    Update compute-node.sh

--- script/compute-node.sh
+++ script/compute-node.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
-./etc/parallelcluster/cfnconfig
+# #!/bin/bash
+# ./etc/parallelcluster/cfnconfig
 
-s3_bucket_name=vishal-application
-region=us-east-1
-S3FileName=pcluster/cw-computenode.json
-dir=cw-configs
+# s3_bucket_name=vishal-application
+# region=us-east-1
+# S3FileName=pcluster/cw-computenode.json
+# dir=cw-configs
 
-# makde dir for logs for jobs belonging to this queue
-mkdir /shared/logs/$stack_name/${cfn_scheduler_queue_name}
+# # makde dir for logs for jobs belonging to this queue
+# mkdir /shared/logs/$stack_name/${cfn_scheduler_queue_name}
 
-cd /shared/$dir
+# cd /shared/$dir
 
-aws s3api get-object --bucket $s3_bucket_name --region $region --key $S3FileName  cw-computenode.json
+# aws s3api get-object --bucket $s3_bucket_name --region $region --key $S3FileName  cw-computenode.json
 
-# replace queue name in file
-sed -i "s/__QUEUE_NAME__/${cfn_scheduler_queue_name}/g"  	cw-computenode.json
+# # replace queue name in file
+# sed -i "s/__QUEUE_NAME__/${cfn_scheduler_queue_name}/g"  	cw-computenode.json
 
-# add config to cloudwatch
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a append-config -m ec2 -s -c file:cw-computenode.json
+# # add config to cloudwatch
+# /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a append-config -m ec2 -s -c file:cw-computenode.json
 
