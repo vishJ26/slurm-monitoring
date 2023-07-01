1a550907fadebfa7fd2075e4e0c6046532b042a8
vishJ26 <99247415+vishJ26@users.noreply.github.com>
2023-07-01 19:03:22 +0530

    Update compute-node.sh

--- script/compute-node.sh
+++ script/compute-node.sh
@@ -4,9 +4,12 @@
 s3_bucket_name=vishal-application
 region=us-east-1
 S3FileName=pcluster/cw-computenode.json
-# dir=cw-configs
+dir=cw-configs
 
-cd /shared/cw-configs
+# makde dir for logs for jobs belonging to this queue
+mkdir /shared/logs/$stack_name/${cfn_scheduler_queue_name}
+
+cd /shared/$dir
 
 aws s3api get-object --bucket $s3_bucket_name --region $region --key $S3FileName  cw-computenode.json
 
@@ -14,5 +17,5 @@ aws s3api get-object --bucket $s3_bucket_name --region $region --key $S3FileName
 sed -i "s/__QUEUE_NAME__/${cfn_scheduler_queue_name}/g"  	cw-computenode.json
 
 # add config to cloudwatch
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a append-config -m ec2 -s -c file:cw-computenode.json
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a append-config -m ec2 -s -c file:cw-computenode.json
 
