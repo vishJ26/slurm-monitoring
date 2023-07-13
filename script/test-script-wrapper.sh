be4a92bc937cdcca0aabea56fb09409e74ee2988
vishJ26 <99247415+vishJ26@users.noreply.github.com>
2023-07-13 16:52:01 +0530

    Create test-script-wrapper.sh

---
+++ script/test-script-wrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Load AWS Parallelcluster environment variables
+. /etc/parallelcluster/cfnconfig
+
+
+test_script=https://raw.githubusercontent.com/vishJ26/slurm-monitoring/master/script/test-script.sh
+
+cd /shared/vishal/scripts
+curl -o test.sh $test_script
+
+#Execute the monitoring installation script
+# bash -x "${monitoring_home}/parallelcluster-setup/${setup_command}" >/tmp/monitoring-setup.log 2>&1
+bash -x test.sh >/tmp/test-script.log 2>&1
+exit $?
