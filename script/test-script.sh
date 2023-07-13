e8636c1ebabe177e16ebafd47b49efae9d0969c6
vishJ26 <99247415+vishJ26@users.noreply.github.com>
2023-07-13 18:47:33 +0530

    Update test-script.sh

--- script/test-script.sh
+++ script/test-script.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-source /etc/parallelcluster/cfnconfig
+# source /etc/parallelcluster/cfnconfig
 
 # get post install arguments
 
 # export S3Bucket="$2"
 # export S3Key="$3"
 # export efadminPassword="$4"
-
-export Argument1="$1"
-export Argument2="$2"
-export Argument2="$3"
-export Argument4="$4"
-
 source '/etc/parallelcluster/cfnconfig'
 
-echo "Argument 1 : ${Argument1}"
-echo "Argument 2 : ${Argument2}"
-echo "Argument 3 : ${Argument3}"
-echo "Argument 4 : ${Argument4}"
+# export Argument1="$1"
+# export Argument2="$2"
+# export Argument2="$3"
+# export Argument4="$4"
+
+
+# echo "Argument 1 : ${Argument1}"
+# echo "Argument 2 : ${Argument2}"
+# echo "Argument 3 : ${Argument3}"
+# echo "Argument 4 : ${Argument4}"
 
 
 queue_name=$cfn_scheduler_queue_name
